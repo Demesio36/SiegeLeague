578bc5587cfb62192ab5447a4061d2a94e912ca9
Demesio36 <demesioserrano89@gmail.com>
2025-06-07 14:55:03 -0500

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,3 @@
+// config.js
+window.SUPABASE_URL = 'https://exrzjcfxgnjffuyhqggx.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4cnpqY2Z4Z25qZmZ1eWhxZ2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMjMwNDEsImV4cCI6MjA2NDg5OTA0MX0.2CIwH4PsQeeTEdNX3v9xpLqEb7Yj1JxM_rQ-WMCN-oY';
