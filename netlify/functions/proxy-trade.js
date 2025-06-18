236cba660bb2729c006d648bb7771273f37e8a67
Demesio36 <demesioserrano89@gmail.com>
2025-06-18 15:18:04 -0500

    Add files via upload

--- netlify/functions/proxy-trade.js
+++ netlify/functions/proxy-trade.js
@@ -11,7 +11,7 @@ export const handler = async (event) => {
   }
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbxK9Q_SgyqXMESycU3WL0XTHzU4WSF5T5hBDgl7Xji5S0ZqVg-9e6FS9DMTPm_G4CO_5Q/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbz4OqaNzVHamo4hp5P0h4tNKeCIIDgokzagVGCJv-nNbl62i1-4U2b2dm98L7uFjeOX4g/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
